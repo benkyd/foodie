1dfd40927efc0108d75205e376c111912d66f690
Ben <36240171+benkyd@users.noreply.github.com>
2022-04-02 12:40:55 +0100

    idk

---
+++ client/src/globals.ts
@@ -0,0 +1,1 @@
+export const SERVER_URL = `0.0.0.0:8080`;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,12 @@
 import express from 'express';
 
+const PORT = 8081;
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
